86e44db6fe523320b0d8b370370b877f19f61b8b
DIGESH <danteasd7@protonmail.com>
2025-10-06 09:33:26 +0530

    add getUserChannelProfile endpoint to fetch user channel details and subscription status

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -305,4 +305,79 @@ const updateUserCoverPage = asyncHandler(async (req, res) => {
         .json(new ApiResponse(400, user, "Cover image updated Successfully."))
 })
 
-export { registerUser, loginUser, logoutUser, refreshAccessToken, changeCurrentPassword, getCurrentUser, updateAccountDetails, updateUserAvatar, updateUserCoverPage }
+const getUserChannelProfile = asyncHandler(async (req, res) => {
+    const { username } = req.param
+
+    if (!username?.trim()) {
+        throw new ApiError(400, "User name is missing.")
+    }
+
+    const channel = await User.aggregate([
+        {
+            $match: {
+                username: username?.toLowerCase()
+            }
+        },
+        {
+            $lookup: {
+                from: "subscriptions",
+                localField: "_id",
+                foreignField: "channel",
+                as: "subscribers"
+            }
+        },
+        {
+            $lookup: {
+                from: "subscriptions",
+                localField: "_id",
+                foreignField: "subscriber",
+                as: "subscribersTo"
+            }
+        },
+        {
+            $addFields: {
+                subscriberCount: {
+                    $size: "$subscribers"
+                },
+                channelSubscribedToCount: {
+                    $size: "$subscribersTo"
+                },
+                isSubscribed: {
+                    $cond:
+                    {
+                        if:
+                        {
+                            $in: [req.user?._id, "$subscribers.subscribers"]
+                        },
+                        then: true,
+                        else: false
+                    }
+                }
+            }
+        },
+        {
+            $project: {
+                fullname: 1,
+                username: 1,
+                subscriberCount: 1,
+                channelSubscribedToCount: 1,
+                isSubscribed: 1,
+                avatar: 1,
+                coverImage: 1,
+                email: 1
+
+            }
+        }
+
+
+    ])
+    console.log(channel)
+
+    if (!channel?.length) {
+        throw new ApiError(404, "channel does not exists.")
+    }
+    return res.status(200)
+        .json(new ApiResponse(200, channel[0], "User channel is fetched."))
+})
+
+export { registerUser, loginUser, logoutUser, refreshAccessToken, changeCurrentPassword, getCurrentUser, updateAccountDetails, updateUserAvatar, updateUserCoverPage, getUserChannelProfile }
